80f9e5ac31ff63bf93edf1b3b613d8cb70a5cd46
arttttt <artem-bambalov@yandex.ru>
2024-08-10 18:06:38 +0300

    update widget

--- app/src/main/java/com/arttttt/bettercalendarwidget/ui/widget/SmallCalendarWidget.kt
+++ app/src/main/java/com/arttttt/bettercalendarwidget/ui/widget/SmallCalendarWidget.kt
@@ -1,18 +1,17 @@
 package com.arttttt.bettercalendarwidget.ui.widget
 
 import android.content.Context
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
-import androidx.glance.Button
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.components.Scaffold
 import androidx.glance.appwidget.provideContent
-import androidx.glance.background
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
 import androidx.glance.layout.Row
+import androidx.glance.layout.fillMaxHeight
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.padding
 import androidx.glance.text.Text
@@ -22,29 +21,35 @@ class SmallCalendarWidget : GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
 
         provideContent {
-            MyContent()
-        }
-    }
+            GlanceTheme {
+                Scaffold {
+                    Row(
+                        modifier = GlanceModifier
+                            .fillMaxSize()
+                            .padding(4.dp),
+                    ) {
+                        Column(
+                            modifier = GlanceModifier
+                                .fillMaxHeight()
+                                .padding(
+                                    horizontal = 12.dp,
+                                ),
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            Text(text = "Tue")
+                            Text("04")
+                            Text("Oct")
+                        }
 
-    @Composable
-    private fun MyContent() {
-        Column(
-            modifier = GlanceModifier
-                .fillMaxSize()
-                .background(GlanceTheme.colors.background),
-            verticalAlignment = Alignment.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "Where to?", modifier = GlanceModifier.padding(12.dp))
-            Row(horizontalAlignment = Alignment.CenterHorizontally) {
-                Button(
-                    text = "Home",
-                    onClick = {}
-                )
-                Button(
-                    text = "Work",
-                    onClick = {}
-                )
+                        Column(
+                            modifier = GlanceModifier.fillMaxHeight(),
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            Text("Calendar event title")
+                            Text("Calendar event details")
+                        }
+                    }
+                }
             }
         }
     }
